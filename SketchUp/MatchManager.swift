f59e5b8baaa73ade5dafb3b2e869fad13a16a8c8
Valerie Reiss <127006059+ValerieReiss@users.noreply.github.com>
2023-03-28 06:00:36 +0200

    Add files via upload

---
+++ SketchUp/MatchManager.swift
@@ -0,0 +1,21 @@
+//
+//  MatchManager.swift
+//  SketchUp
+//
+//  Created by Valerie on 28.03.23.
+//
+
+import Foundation
+
+class MatchManager: ObservableObject {
+    @Published var inGame = false
+    @Published var isGameOver = false
+    @Published var authenticationState = PlayerAuthState.authenticating
+    
+    @Published var currentlyDrawing = false
+    @Published var drawPrompt = ""
+    @Published var pastGuesses = [PastGuess]()
+    
+    @Published var score = 0
+    @Published var remainingTime = maxTimeRemaining
+}
